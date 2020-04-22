86a90e45a3926f9eeb7709082d1b559452ceea3d
cshen1pin <chunyishen8672@gmail.com>
2020-04-22 08:49:26 +0800

    first commit

---
+++ runcamera.sh
@@ -0,0 +1,1 @@
+python3 imagenet-camera.py --prototxt=/home/choyen/lenet/deploy.prototxt --network=/home/choyen/lenet/snapshot_iter_180.caffemodel --labels=/home/choyen/lenet/labels.txt --input_blob=data --output_blob=softmax
